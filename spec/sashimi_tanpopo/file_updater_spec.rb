79071a8c3def09cdb69ef3730b58ec0b23b65f0d
sue445 <sue445@sue445.net>
2025-10-11 23:53:19 +0900

    Add spec

--- spec/sashimi_tanpopo/file_updater_spec.rb
+++ spec/sashimi_tanpopo/file_updater_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe SashimiTanpopo::FileUpdater do
     let(:dry_run) { false }
     let(:is_colored) { true }
 
-    context "simple case" do
+    context "update single file" do
       let(:recipe_body) do
         <<~RUBY
           update_file "test.txt" do |content|
@@ -47,6 +47,33 @@ RSpec.describe SashimiTanpopo::FileUpdater do
 
         test_txt = File.read(temp_dir_path.join("test.txt"))
         expect(test_txt).to eq "Hi, sue445!\n"
+      end
+    end
+
+    context "update multiple files" do
+      let(:recipe_body) do
+        <<~RUBY
+          update_file "*.txt" do |content|
+            content.gsub!("name", params[:name])
+          end
+        RUBY
+      end
+
+      let(:params) { { name: "sue445"} }
+
+      before do
+        FileUtils.cp(fixtures_dir.join("test.txt"), temp_dir)
+        FileUtils.cp(fixtures_dir.join("test2.txt"), temp_dir)
+      end
+
+      it "files are updated" do
+        subject
+
+        test_txt = File.read(temp_dir_path.join("test.txt"))
+        expect(test_txt).to eq "Hi, sue445!\n"
+
+        test_txt2 = File.read(temp_dir_path.join("test2.txt"))
+        expect(test_txt2).to eq "Hello, sue445!\n"
       end
     end
   end
