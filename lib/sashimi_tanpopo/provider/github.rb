b2fb33bb36d7bc25736aa50de9a7e9d5f5638964
sue445 <sue445@sue445.net>
2025-10-12 21:50:10 +0900

    Impl create_branch_and_push_changes

--- lib/sashimi_tanpopo/provider/github.rb
+++ lib/sashimi_tanpopo/provider/github.rb
@@ -8,7 +8,24 @@ module SashimiTanpopo
       # @param params [Hash<Symbol, String>]
       # @param dry_run [Boolean]
       # @param is_colored [Boolean] Whether show color diff
-      def initialize(recipe_paths:, target_dir:, params:, dry_run:, is_colored:)
+      # @param git_username [String]
+      # @param git_email [String]
+      # @param commit_message [String]
+      # @param repository [String]
+      # @param access_token [String]
+      # @param api_endpoint [String]
+      # @param pr_title [String]
+      # @param pr_body [String]
+      # @param pr_source_branch [String] PullRequest source branch (a.k.a. head branch)
+      # @param pr_target_branch [String] PullRequest target branch (a.k.a. base branch)
+      # @param pr_assignees [Array<String>]
+      # @param pr_reviewers [Array<String>]
+      # @param pr_labels [Array<String>]
+      def initialize(recipe_paths:, target_dir:, params:, dry_run:, is_colored:,
+                     git_username:, git_email:, commit_message:,
+                     repository:, access_token:, api_endpoint: "https://api.github.com/",
+                     pr_title:, pr_body:, pr_source_branch:, pr_target_branch:,
+                     pr_assignees: [], pr_reviewers: [], pr_labels: [])
         super(
           recipe_paths: recipe_paths,
           target_dir:   target_dir,
@@ -16,12 +33,71 @@ module SashimiTanpopo
           dry_run:      dry_run,
           is_colored:   is_colored,
         )
+
+        @git_username = git_username
+        @git_email = git_email
+        @commit_message = commit_message
+        @repository = repository
+        @pr_title = pr_title
+        @pr_body = pr_body
+        @pr_source_branch = pr_source_branch
+        @pr_target_branch = pr_target_branch
+        @pr_assignees = pr_assignees
+        @pr_reviewers = pr_reviewers
+        @pr_labels = pr_labels
+
+        @client = Octokit::Client.new(api_endpoint: api_endpoint, access_token: access_token)
       end
 
       def perform
-        apply_recipe_files
+        changed_file_paths = apply_recipe_files
 
-        # TODO
+        return if changed_file_paths.empty? || @dry_run
+
+        create_branch_and_push_changes(changed_file_paths)
+
+        # TODO: Impl
+        # TODO: restore files
+      end
+
+      private
+
+      # Create branch on repository and push changes
+      def create_branch_and_push_changes(changed_file_paths)
+        current_ref = @client.ref(@repository, "heads/#{@pr_target_branch}")
+        branch_ref = @client.create_ref(@repository, "heads/#{@pr_source_branch}", current_ref.object.sha)
+
+        branch_commit = @client.commit(@repository, branch_ref.object.sha)
+
+        tree_metas = changed_file_paths.map { |file_path| create_tree_meta(file_path) }
+        tree = @client.create_tree(@repository, tree_metas, base_tree: branch_commit.commit.tree.sha)
+
+        commit = @client.create_commit(
+          @repository,
+          @commit_message,
+          tree.sha,
+          branch_ref.object.sha,
+          author: {
+            name: @git_username,
+            email: @git_email,
+          }
+        )
+
+        @client.update_ref(@repository, "heads/#{@pr_source_branch}", commit.sha)
+      end
+
+      # @param file_path [String]
+      def create_tree_meta(file_path)
+        full_file_path = File.join(@target_dir, file_path)
+        file_body = File.read(full_file_path)
+        file_body_sha = @client.create_blob(@repository, file_body)
+
+        {
+          path: file_path,
+          mode: File.stat(full_file_path).mode.to_s(8),
+          type: "blob",
+          sha:  file_body_sha,
+        }
       end
     end
   end
--- spec/sashimi_tanpopo/provider/github_spec.rb
+++ spec/sashimi_tanpopo/provider/github_spec.rb
@@ -3,11 +3,23 @@
 RSpec.describe SashimiTanpopo::Provider::GitHub do
   let(:provider) do
     SashimiTanpopo::Provider::GitHub.new(
-      recipe_paths: recipe_paths,
-      target_dir:   target_dir,
-      params:       params,
-      dry_run:      dry_run,
-      is_colored:   is_colored,
+      recipe_paths:     recipe_paths,
+      target_dir:       target_dir,
+      params:           params,
+      dry_run:          dry_run,
+      is_colored:       is_colored,
+      git_username:     git_username,
+      git_email:        git_email,
+      commit_message:   commit_message,
+      repository:       repository,
+      access_token:     access_token,
+      pr_title:         pr_title,
+      pr_body:          pr_body,
+      pr_source_branch: pr_source_branch,
+      pr_target_branch: pr_target_branch,
+      pr_assignees:     pr_assignees,
+      pr_reviewers:     pr_reviewers,
+      pr_labels:        pr_labels,
     )
   end
 
@@ -18,6 +30,19 @@ RSpec.describe SashimiTanpopo::Provider::GitHub do
   let(:params) { {} }
   let(:dry_run) { false }
   let(:is_colored) { true }
+
+  let(:git_username)     { "test" }
+  let(:git_email)        { "test@example.com" }
+  let(:commit_message)   { "Update files" }
+  let(:repository)       { "example/example" }
+  let(:access_token)     { "DUMMY" }
+  let(:pr_title)         { "PR title" }
+  let(:pr_body)          { "PR body" }
+  let(:pr_source_branch) { "test" }
+  let(:pr_target_branch) { "main" }
+  let(:pr_assignees)     { %w(sue445) }
+  let(:pr_reviewers)     { %w(sue445) }
+  let(:pr_labels)        { %w(sashimi-tanpopo) }
 
   describe "#perform" do
     subject { provider.perform }
@@ -34,7 +59,90 @@ RSpec.describe SashimiTanpopo::Provider::GitHub do
 
     let(:params) { { name: "sue445"} }
 
-    it "file is updated" do
+    let(:request_headers) do
+      {
+        "Accept" => "application/vnd.github.v3+json",
+        "Authorization" => "token #{access_token}",
+        "Content-Type" => "application/json",
+      }
+    end
+
+    let(:response_headers) do
+      {
+        "Content-Type" => "application/json",
+      }
+    end
+
+    before do
+      stub_request(:get, "https://api.github.com/repos/#{repository}/git/refs/heads/#{pr_target_branch}").
+        with(headers: request_headers).
+        to_return(status: 200, headers: response_headers, body: fixture("github_get_ref.json"))
+
+      ref_json = {
+        ref: "refs/heads/#{pr_source_branch}",
+        sha: "aa218f56b14c9653891f9e74264a383fa43fefbd",
+      }.to_json
+
+      stub_request(:post, "https://api.github.com/repos/#{repository}/git/refs").
+        with(headers: request_headers, body: ref_json).
+        to_return(status: 201, headers: response_headers, body: fixture("github_create_ref.json"))
+
+      stub_request(:get, "https://api.github.com/repos/#{repository}/commits/aa218f56b14c9653891f9e74264a383fa43fefbd").
+        with(headers: request_headers).
+        to_return(status: 200, headers: response_headers, body: fixture("github_get_commit.json"))
+
+      blob_json = {
+        content: "Hi, sue445!\n",
+        encoding: "utf-8",
+      }.to_json
+
+      stub_request(:post, "https://api.github.com/repos/#{repository}/git/blobs").
+        with(headers: request_headers, body: blob_json).
+        to_return(status: 201, headers: response_headers, body: fixture("github_create_blob.json"))
+
+      tree_json = {
+        base_tree: "6dcb09b5b57875f334f61aebed695e2e4193db5e",
+        tree: [
+          {
+            path: "test.txt",
+            mode: "100644",
+            type: "blob",
+            sha: "3a0f86fb8db8eea7ccbb9a95f325ddbedfb25e15"
+          }
+        ]
+      }.to_json
+
+      stub_request(:post, "https://api.github.com/repos/#{repository}/git/trees").
+        with(headers: request_headers, body: tree_json).
+        to_return(status: 201, headers: response_headers, body: fixture("github_create_tree.json"))
+
+      commit_json = {
+        author: {
+          name: "test",
+          email:  "test@example.com",
+        },
+        message: "Update files",
+        tree: "cd8274d15fa3ae2ab983129fb037999f264ba9a7",
+        parents: [
+          "aa218f56b14c9653891f9e74264a383fa43fefbd",
+        ],
+      }.to_json
+
+      stub_request(:post, "https://api.github.com/repos/#{repository}/git/commits").
+        with(headers: request_headers, body: commit_json).
+        to_return(status: 201, headers: response_headers, body: fixture("github_create_commit.json"))
+
+      update_ref_json = {
+        sha: "7638417db6d59f3c431d3e1f261cc637155684cd",
+        force: false,
+      }.to_json
+
+      stub_request(:patch, "https://api.github.com/repos/#{repository}/git/refs/heads/#{pr_source_branch}").
+        with(headers: request_headers, body: update_ref_json).
+        to_return(status: 200, headers: response_headers, body: fixture("github_update_ref.json"))
+    end
+
+    it "file is updated and create PullRequest" do
       subject
 
       test_txt = File.read(temp_dir_path.join("test.txt"))
