6f25bf8d4ec2b93bef74e9fa34d5bb5ff3b0906c
sue445 <sue445@sue445.net>
2025-11-02 01:19:46 +0900

    Add logger_spec

---
+++ spec/sashimi_tanpopo/logger_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+RSpec.describe SashimiTanpopo::Logger do
+  describe ".info" do
+    subject do
+      SashimiTanpopo.logger.info(message)
+      log_output.string
+    end
+
+    include_context :stub_logger
+
+    context "normal" do
+      let(:message) { "test" }
+
+      it { should eq " INFO : test\n" }
+    end
+
+    context "End with newline" do
+      let(:message) { "test\n" }
+
+      it { should eq " INFO : test\n" }
+    end
+  end
+end
